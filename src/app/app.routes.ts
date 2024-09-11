0d7a11c9ac6211f3b616611ca3a87c6779a3c118
Alejandro <alejandro.flores.a@ucb.edu.bo>
2024-09-11 14:09:17 -0400

    register-customer form added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { RegisterCustomerComponent } from './components/user/register-customer/register-customer.component';
+import { RegisterLibrarianComponent } from './components/user/register-librarian/register-librarian.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'register-customer', component: RegisterCustomerComponent},
+    {path: 'register-librarian', component: RegisterLibrarianComponent}
+];
--- src/app/components/user/register-customer/register-customer.component.ts
+++ src/app/components/user/register-customer/register-customer.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register-customer',
@@ -8,5 +9,27 @@ import { Component } from '@angular/core';
   styleUrl: './register-customer.component.css'
 })
 export class RegisterCustomerComponent {
+  registerForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.registerForm = this.fb.group({
+      nombre: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      telefono: ['', Validators.required],
+      direccion: ['', Validators.required]
+    });
+  }
+
+  onSubmit() {
+    if (this.registerForm.valid) {
+      console.log(this.registerForm.value);
+      // Aquí puedes añadir la lógica para enviar los datos
+    }
+  }
+
+  onGoogleAuth() {
+    // Lógica para autenticación con Google
+    console.log('Autenticación con Google');
+  }
 
 }
