6b0e503358c1ce7c05d0d22973186c427ad6f7f0
Alejandro <alejandro.flores.a@ucb.edu.bo>
2024-09-11 13:12:07 -0400

    register-customer component added

---
+++ src/app/components/user/register-customer/register-customer.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-register-customer',
+  standalone: true,
+  imports: [],
+  templateUrl: './register-customer.component.html',
+  styleUrl: './register-customer.component.css'
+})
+export class RegisterCustomerComponent {
+
+}
