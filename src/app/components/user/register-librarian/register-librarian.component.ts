0bb4738518df808e9d4aac88554bcb168c0ddfc7
Alejandro <alejandro.flores.a@ucb.edu.bo>
2024-09-11 13:46:37 -0400

    register-librarian component added

---
+++ src/app/components/user/register-librarian/register-librarian.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-register-librarian',
+  standalone: true,
+  imports: [],
+  templateUrl: './register-librarian.component.html',
+  styleUrl: './register-librarian.component.css'
+})
+export class RegisterLibrarianComponent {
+
+}
