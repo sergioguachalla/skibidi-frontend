4cab3dfb5f3def760e6dc4b2aeb4b7a26a482789
Alejandro <alejandro.flores.a@ucb.edu.bo>
2024-09-18 15:37:18 -0400

    validators added to register-customer

--- src/app/components/user/register-customer/register-customer.component.ts
+++ src/app/components/user/register-customer/register-customer.component.ts
@@ -1,34 +1,47 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { NavbarComponent } from '../../shared/navbar/navbar.component';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-register-customer',
   standalone: true,
-  imports: [NavbarComponent],
+  imports: [NavbarComponent, CommonModule, ReactiveFormsModule],
   templateUrl: './register-customer.component.html',
-  styleUrl: './register-customer.component.css'
+  styleUrls: ['./register-customer.component.css']
 })
 export class RegisterCustomerComponent {
   registerForm: FormGroup;
 
-  constructor(private fb: FormBuilder) {
-    this.registerForm = this.fb.group({
-      nombre: ['', Validators.required],
+  constructor(private formBuilder: FormBuilder) {
+    this.registerForm = this.formBuilder.group({
+      nombres: ['', Validators.required],
+      apellidos: ['', Validators.required],
       email: ['', [Validators.required, Validators.email]],
-      telefono: ['', Validators.required],
-      direccion: ['', Validators.required]
+      celular: ['', [Validators.required, ]], // ejemplo de validación para números de celular de 10 dígitos
+      direccion: ['', Validators.required],
+      username: ['', Validators.required],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      confirmPassword: ['', Validators.required]
+    }, {
+      validator: this.passwordMatchValidator // Validador personalizado para confirmar que las contraseñas coinciden
     });
+  }
+
+  passwordMatchValidator(form: FormGroup) {
+    const password = form.get('password')?.value;
+    const confirmPassword = form.get('confirmPassword')?.value;
+
+    return password === confirmPassword ? null : { mismatch: true };
   }
 
   onSubmit() {
     if (this.registerForm.valid) {
-      console.log(this.registerForm.value);
+      console.log('Formulario válido:', this.registerForm.value);
+    } else {
+      console.log('Formulario no válido');
+      this.registerForm.markAllAsTouched(); // Esto marca todos los campos como tocados para mostrar los errores
     }
   }
-
-  onGoogleAuth() {
-    console.log('Autenticación con Google');
-  }
-
 }
