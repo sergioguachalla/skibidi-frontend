13f3fad1d208283a5552e0eb18d86cdc6076f9a8
Alejandro <alejandro.flores.a@ucb.edu.bo>
2024-09-26 22:45:49 -0400

    environment request client added

---
+++ src/app/components/environment-client/environment-client.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { EnvironmentClientComponent } from './environment-client.component';
+
+describe('EnvironmentClientComponent', () => {
+  let component: EnvironmentClientComponent;
+  let fixture: ComponentFixture<EnvironmentClientComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [EnvironmentClientComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(EnvironmentClientComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/components/environment-client/environment-client.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-environment-client',
+  standalone: true,
+  imports: [],
+  templateUrl: './environment-client.component.html',
+  styleUrl: './environment-client.component.css'
+})
+export class EnvironmentClientComponent {
+
+}
