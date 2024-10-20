c79319f8341a9c721a5857d9801049d456eaeae7
Alejandro <alejandro.flores.a@ucb.edu.bo>
2024-10-20 17:49:47 -0400

    logout bug fixed

--- src/app/components/shared/navbar/navbar.component.ts
+++ src/app/components/shared/navbar/navbar.component.ts
@@ -18,9 +18,11 @@ export class NavbarComponent {
     this.keycloakService.login();
   }
   logout() {
-    this.keycloakService.logout();
-    this.router.navigate(['']);
+    this.router.navigate(['/'], { queryParams: { logout: 'true' } });
+
   }
+
+
 
   onClickToHome() {
     this.router.navigate(['/view-book']);
@@ -28,6 +30,9 @@ export class NavbarComponent {
 
   onClickToEnvReservation() {
     this.router.navigate(['/reservation-enviroment']);
+  }
+  onClickToEnvReservationClient() {
+    this.router.navigate(['/client-environment']);
   }
 
   onClickToReservations () {
