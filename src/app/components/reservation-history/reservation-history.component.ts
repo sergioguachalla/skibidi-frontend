e3a2acb35080bbd35df8c20a4c45cdbf2e49c979
Alejandro <alejandro.flores.a@ucb.edu.bo>
2024-10-22 10:35:13 -0400

    pagination added to reservation-history

--- src/app/components/reservation-history/reservation-history.component.ts
+++ src/app/components/reservation-history/reservation-history.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from '../shared/navbar/navbar.component';
 import { ReservationHistoryService } from '../../services/reservation-history.service';
+import { Router } from "@angular/router";
 
 @Component({
   selector: 'app-reservation-history',
@@ -15,21 +16,53 @@ import { ReservationHistoryService } from '../../services/reservation-history.se
 })
 export class ReservationHistoryComponent implements OnInit {
   reservations: any[] = [];
+  currentPage: number = 0;
+  totalPages: number = 0;
+  pageSize: number = 7;
 
-  constructor(private reservationHistoryService: ReservationHistoryService) { }
+  constructor(
+    private reservationHistoryService: ReservationHistoryService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
-    this.getReservationHistory();
+    this.getReservationHistory(this.currentPage);
   }
 
-  getReservationHistory(): void {
-    this.reservationHistoryService.getReservationHistory().subscribe(
+  getReservationHistory(page: number): void {
+    this.reservationHistoryService.getReservationHistory(page, 2).subscribe(
       (response) => {
-        this.reservations = response.data || [];
+        this.reservations = response.data.content || [];
+        this.currentPage = response.data.number;
+        this.totalPages = response.data.totalPages;
       },
       (error) => {
         console.error('Error al obtener el historial de reservas:', error);
       }
     );
+  }
+
+  nextPage(): void {
+    if (this.currentPage < this.totalPages - 1) {
+      this.getReservationHistory(this.currentPage + 1);
+    }
+  }
+
+  previousPage(): void {
+    if (this.currentPage > 0) {
+      this.getReservationHistory(this.currentPage - 1);
+    }
+  }
+
+  addReservation() {
+    this.router.navigate(['/client-environment']);
+  }
+
+  editReservation(reservation: any): void {
+    this.router.navigate(['/client-environment/edit', reservation.id]);
+  }
+
+  deleteReservation(reservation: any): void {
+    this.router.navigate(['/client-environment/edit', reservation.id]);
   }
 }
