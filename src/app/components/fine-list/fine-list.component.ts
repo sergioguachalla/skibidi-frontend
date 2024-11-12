20c9b8719b3b344adaa1356cab4da0c9970a1080
Sergio Guachalla <sergioguachalla0@gmail.com>
2024-11-12 14:07:32 -0400

    add ine list component and service

--- src/app/components/fine-list/fine-list.component.ts
+++ src/app/components/fine-list/fine-list.component.ts
@@ -1,109 +1,15 @@
-import {Component, inject, OnInit} from '@angular/core';
+import { Component } from '@angular/core';
 import {NavbarComponent} from "../shared/navbar/navbar.component";
-import {FinesService} from "../../services/fines.service";
-import {NgClass, NgForOf, NgIf} from "@angular/common";
-import {toArray} from "rxjs";
-import {FormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-fine-list',
   standalone: true,
   imports: [
-    NavbarComponent,
-    NgForOf,
-    NgIf,
-    NgClass,
-    FormsModule
+    NavbarComponent
   ],
   templateUrl: './fine-list.component.html',
   styleUrl: './fine-list.component.css'
 })
-export class FineListComponent implements OnInit {
-  fines: any = [];
-  fineDetail: any;
-  currentPage: number = 0;
-  totalPages: number = 0;
-  pagesArray: number[] = [];
-  isModalOpen: boolean = false;
-  private fineService: FinesService = inject(FinesService);
-  startDate: any;
-  endDate: any;
+export class FineListComponent {
 
-  constructor() {}
-
-  ngOnInit(): void {
-    this.findAllFines();
-  }
-
-  findAllFines(page: number = 0) {
-    this.fineService.findAll(page, 5, null, null, null, null).subscribe((response) => {
-      this.fines = response.data.content;
-      this.currentPage = response.data.pageable.pageNumber;
-      this.totalPages = response.data.totalPages;
-      this.pagesArray = this.getPagesSubset(this.currentPage, this.totalPages);
-    });
-  }
-
-  onPageChange(page: number) {
-    this.currentPage = page;
-    this.findAllFines(page);
-  }
-
-  getPagesSubset(current: number, total: number): number[] {
-    const maxVisiblePages = 5;
-    let startPage = Math.max(current - Math.floor(maxVisiblePages / 2), 0);
-    const endPage = Math.min(startPage + maxVisiblePages, total);
-
-    if (endPage - startPage < maxVisiblePages) {
-      startPage = Math.max(endPage - maxVisiblePages, 0);
-    }
-
-    return Array.from({ length: endPage - startPage }, (_, i) => startPage + i);
-  }
-  showFineDetail(fineId: number){
-    this.showModal();
-    this.fineService.findFineDetail(fineId).subscribe((response) => {
-      console.log(response);
-      this.fineDetail = response.data;
-    });
-  }
-
-  closeModal(): void {
-    this.isModalOpen = false;
-  }
-  showModal(): void {
-    this.isModalOpen = true;
-  }
-
-  filterFines() {
-    if (this.startDate && this.endDate) {
-      const startDate = new Date(this.startDate);
-      const endDate = new Date(this.endDate);
-
-      const formattedStartDate = startDate.toISOString().split('T')[0];
-      const formattedEndDate = endDate.toISOString().split('T')[0];
-
-      this.fineService.findAll(this.currentPage, 5, null, null, formattedStartDate, formattedEndDate).subscribe((response) => {
-        this.fines = response.data.content;
-        this.currentPage = response.data.pageable.pageNumber;
-        this.totalPages = response.data.totalPages;
-        this.pagesArray = this.getPagesSubset(this.currentPage, this.totalPages);
-      });
-    }
-  }
-
-  clearFilter() {
-    this.startDate = null;
-    this.endDate = null;
-    this.findAllFines();
-  }
-
-
-  payFine(fineId: number) {
-    confirm('Desea registrar el pago de la multa?');
-    this.fineService.payFine(fineId).subscribe(() => {
-      this.findAllFines();
-    });
-
-  }
 }
--- src/app/services/fines.service.ts
+++ src/app/services/fines.service.ts
@@ -1,47 +1,9 @@
-import {inject, Injectable} from '@angular/core';
-import {Development} from "../environments/development";
-import {HttpClient} from "@angular/common/http";
+import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FinesService {
 
-  private API_URL = `${Development.API_URL}/fines/`;
-  private http: HttpClient = inject(HttpClient);
   constructor() { }
-
-  findAll(page: number, size: number, isPaid: boolean | null, userKcId: string | null, startDate: string | null, endDate: string | null) {
-    if (isPaid != null) {
-      return this.http.get<any>(`${this.API_URL}?page=${page}&size=${size}&isPaid=${isPaid}`);
-  }
-    if (userKcId != null) {
-      return this.http.get<any>(`${this.API_URL}?page=${page}&size=${size}&userKcId=${userKcId}`);
-    }
-    if (startDate != null && endDate != null) {
-      return this.http.get<any>(`${this.API_URL}?page=${page}&size=${size}&startDate=${startDate}&endDate=${endDate}`);
-    }
-    return this.http.get<any>(`${this.API_URL}?page=${page}&size=${size}`);
-  }
-
-  findFineDetail(fineId: number) {
-    return this.http.get<any>(`${this.API_URL}details/${fineId}`);
-  }
-
-  findPaidFines(page: number, size: number, username: string | null) {
-    if (username != null) {
-      return this.http.get<any>(`${this.API_URL}paid?page=${page}&size=${size}&username=${username}`);
-    }
-    return this.http.get<any>(`${this.API_URL}paid?page=${page}&size=${size}`);
-  }
-
-  payFine(fineId: number) {
-    return this.http.put<any>(`${this.API_URL}pay/${fineId}`, null);
-  }
-
-
-
-
-
-
 }
