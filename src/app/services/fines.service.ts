07d80f564a00d49d3706ab1e0198fd363a240142
Sergio Guachalla <sergioguachalla0@gmail.com>
2024-11-12 14:36:13 -0400

    add backend connection

--- src/app/services/fines.service.ts
+++ src/app/services/fines.service.ts
@@ -1,9 +1,25 @@
-import { Injectable } from '@angular/core';
+import {inject, Injectable} from '@angular/core';
+import {Development} from "../environments/development";
+import {HttpClient} from "@angular/common/http";
 
 @Injectable({
   providedIn: 'root'
 })
 export class FinesService {
 
+  private API_URL = `${Development.API_URL}/fines/`;
+  private http: HttpClient = inject(HttpClient);
   constructor() { }
+
+  findAll(page: number, size: number, isPaid: boolean | null){
+    if (isPaid != null) {
+      return this.http.get<any>(`${this.API_URL}?page=${page}&size=${size}&isPaid=${isPaid}`);
+  }
+    return this.http.get<any>(`${this.API_URL}?page=${page}&size=${size}`);
+  }
+
+
+
+
+
 }
