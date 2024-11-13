c11cec76e3b1112954a6fef8be32e0c0ad56fca0
Sergio Guachalla <sergioguachalla0@gmail.com>
2024-11-13 13:07:24 -0400

    add client list component

--- src/app/components/client-list/client-list.component.ts
+++ src/app/components/client-list/client-list.component.ts
@@ -1,19 +1,28 @@
 import {Component, inject} from '@angular/core';
 import {NavbarComponent} from "../shared/navbar/navbar.component";
 import {UserClientService} from "../../services/userclient.service";
+<<<<<<< HEAD
 import {NgClass, NgForOf, NgIf} from "@angular/common";
 import {FinesService} from "../../services/fines.service";
 import {FormsModule} from "@angular/forms";
+=======
+import {NgForOf} from "@angular/common";
+import {FinesService} from "../../services/fines.service";
+>>>>>>> c698002 (add client list component)
 
 @Component({
   selector: 'app-client-list',
   standalone: true,
   imports: [
     NavbarComponent,
+<<<<<<< HEAD
     NgForOf,
     NgIf,
     NgClass,
     FormsModule
+=======
+    NgForOf
+>>>>>>> c698002 (add client list component)
   ],
   templateUrl: './client-list.component.html',
   styleUrl: './client-list.component.css'
@@ -23,46 +32,22 @@ export class ClientListComponent {
   private userClientService: UserClientService = inject(UserClientService);
   private fineService: FinesService = inject(FinesService);
   protected clients: any[] = [];
-  protected searchTerm: string = '';
   selectedClient: any;
   debts: any[] = [];
   isModalOpen: boolean = false;
-  isValidSearchTerm: boolean = true;
-
   constructor() {
     this.findAllClients();
   }
 
   findAllClients() {
-    this.userClientService.getAllUserClients(null).subscribe((response) => {
+    this.userClientService.getAllUserClients().subscribe((response) => {
       this.clients = response.data;
     });
   }
 
-  onConsultDebts(userKcId: string): void {
-    this.selectedClient = this.clients.find(client => client.kcUuid === userKcId);
-    this.fineService.findAll(0,10,null, userKcId, null, null).subscribe((response) => {
-      this.debts = response.data.content;
-      this.isModalOpen = true;
-    });
-  }
-
-  closeModal(): void {
-    this.isModalOpen = false;
-  }
-  onSearchTermChange() {
-    this.searchTerm = this.searchTerm.trim();
-
-    const validPattern = /^[a-zA-Z0-9\s]*$/;
-    this.isValidSearchTerm = validPattern.test(this.searchTerm);
-  }
-
-  searchClient() {
-    if (!this.isValidSearchTerm) {
-      return;
-    }
-    this.userClientService.getAllUserClients(this.searchTerm).subscribe((response) => {
-      this.clients = response.data;
+  onConsultDebt(userKcId: string) {
+    this.fineService.findAll(0, 10, null, userKcId).subscribe((response) => {
+      console.log(response);
     });
   }
 }
