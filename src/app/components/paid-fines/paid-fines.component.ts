779a2181f57a31adc7dd9b3d508ecd3f670f0eaf
Sergio Guachalla <sergioguachalla0@gmail.com>
2024-11-19 11:11:54 -0400

    add paid fines component

--- src/app/components/paid-fines/paid-fines.component.ts
+++ src/app/components/paid-fines/paid-fines.component.ts
@@ -1,8 +1,7 @@
 import {Component, inject} from '@angular/core';
 import {NavbarComponent} from "../shared/navbar/navbar.component";
 import {FinesService} from "../../services/fines.service";
-import {CurrencyPipe, DatePipe, NgForOf, NgIf} from "@angular/common";
-import {FormsModule} from "@angular/forms";
+import {CurrencyPipe, DatePipe} from "@angular/common";
 
 @Component({
   selector: 'app-paid-fines',
@@ -10,10 +9,7 @@ import {FormsModule} from "@angular/forms";
   imports: [
     NavbarComponent,
     CurrencyPipe,
-    DatePipe,
-    NgForOf,
-    NgIf,
-    FormsModule
+    DatePipe
   ],
   templateUrl: './paid-fines.component.html',
   styleUrl: './paid-fines.component.css'
@@ -21,54 +17,33 @@ import {FormsModule} from "@angular/forms";
 export class PaidFinesComponent {
 
   private fineService: FinesService = inject(FinesService);
-  protected paidFines: any = [];
-  private page: number = 0;
-  private size: number = 5;
-  currentPage = 1;
-  usernameQuery = '';
 
-  totalPages = 0;
+  private paidFines: any = [];
+
+  private page: number = 0;
+  private size: number = 10;
+
+  currentPage: number = 1;
+  itemsPerPage: number = 5;
   constructor() {
-    this.findPaidFines(this.page, this.size);
-    this.updateTotalPages();
+    this.findPaidFines()
   }
 
-  findPaidFines(page:number, size: number) {
-    page = this.page;
-    size = this.size;
-
-    this.fineService.findPaidFines(this.page, this.size,null).subscribe((response) => {
-      this.paidFines = response.data.content;
-      this.totalPages = response.data.totalPages;
-      console.log(response.data.content)
+  findPaidFines() {
+    this.fineService.findPaidFines(this.page, this.size).subscribe((response) => {
+      this.paidFines = response
+      console.log(response)
     });
   }
 
-
-  updateTotalPages() {
-    this.totalPages = Math.ceil(this.paidFines.length / this.size);
-  }
 
   get paginatedFines() {
-    const startIndex = (this.currentPage - 1) * this.size;
-    const endIndex = startIndex + this.size;
-    return this.paidFines.slice(startIndex, endIndex);
+    const start = (this.currentPage - 1) * this.itemsPerPage;
+    const end = start + this.itemsPerPage;
+    return this.paidFines.slice(start, end);
   }
 
-  changePage(page: number) {
-    if (page >= 1 && page <= this.totalPages) {
-      this.currentPage = page;
-      this.findPaidFines(page, this.size);
-    }
+  onPageChange(page: number) {
+    this.currentPage = page;
   }
-  searchUsername() {
-    if (this.usernameQuery === '') {
-      this.findPaidFines(this.page, this.size);
-    }
-    this.fineService.findPaidFines(this.page, this.size, this.usernameQuery).subscribe((response) => {
-      this.paidFines = response.data.content;
-      this.totalPages = response.data.totalPages;
-    });
-  }
-
 }
