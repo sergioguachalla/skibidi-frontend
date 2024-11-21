01ef6c9c32a77830090f5e9156659290b31ad3dc
Sergio Guachalla <sergioguachalla0@gmail.com>
2024-11-21 18:43:15 -0400

    add delete to fine service

--- src/app/components/type-fine/type-fine.component.ts
+++ src/app/components/type-fine/type-fine.component.ts
@@ -22,7 +22,8 @@ export class TypeFineComponent {
   selectedFine : any;
   typeFines: any[] = [];
   isEditModalOpen: boolean = false;
-
+  isSaveModalOpen: boolean = false;
+  error = null;
   constructor() {
     this.findAllTypeFines();
   }
@@ -37,23 +38,43 @@ export class TypeFineComponent {
   openEditModal(fine: any) {
     this.isEditModalOpen = true;
     this.selectedFine = { ...fine };
-    const modalElement = document.getElementById('editFineModal');
-    if (modalElement) {
-      // @ts-ignore
-      const bootstrapModal = new bootstrap.Modal(modalElement);
-      bootstrapModal.show();
-    }
+
   }
+  openCreateModal() {
+    this.isSaveModalOpen = true;
+    this.selectedFine = { name: '', amount: 0, description: '' };
+  }
+
 
 
   cancelEdit() {
     this.selectedFine = null;
     this.isEditModalOpen = false;
   }
+
   saveEdit() {
     this.typeFineService.updateFine(this.selectedFine).subscribe(() => {
       this.findAllTypeFines();
       this.cancelEdit();
     });
+  }
+  saveTypeFine() {
+    this.typeFineService.saveFine(this.selectedFine).subscribe(() => {
+      this.findAllTypeFines();
+      this.isSaveModalOpen = false;
+    });
+  }
+
+  deleteFine(id: number) {
+    alert('Esta seguro de eliminar el tipo de multa?');
+    this.typeFineService.deleteFine(id).subscribe(
+      response => {
+        this.findAllTypeFines();
+      },
+      error => {
+        alert('No se puede eliminar el tipo de multa');
+        this.error = error;
+      }
+      );
   }
 }
